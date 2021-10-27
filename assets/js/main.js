e9c21d95c8d42660fe98abbf5b288bff388c1748
najimsadat <saidnajimullahs@yahoo.com>
2021-10-27 17:12:12 +0200

    Mobile nav added

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+// Show menu hidden in Y direction -100% bottom
+
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+
+// check if show-menu exsits
+
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+// remove mobile menu
+
+const navLink = document.querySelectorAll(".nav__link");
+
+// each time that function runs closes the nav-menu
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  //   when click on each link on menu removes the show-menu class
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
