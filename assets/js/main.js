0a52d5c1a755c9ba577bb1a8241787228c291433
najimsadat <saidnajimullahs@yahoo.com>
2021-11-12 19:51:53 +0100

    skills added added

--- assets/js/main.js
+++ assets/js/main.js
@@ -143,6 +143,26 @@ themeButton.addEventListener("click", () => {
   localStorage.setItem("selected-icon", getCurrentIcon());
 });
 
+// ------------------Skills toggle
+
+const skillsContent = document.getElementsByClassName("skills__content");
+const skillsHeader = document.querySelectorAll(".skills__header");
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills__content skills__close";
+  }
+  if (itemClass == "skills__content skills__close") {
+    this.parentNode.className = "skills__content skills__open";
+  }
+}
+
+skillsHeader.forEach((el) => {
+  el.addEventListener("click", toggleSkills);
+});
+
 // ------------------Scroll Reveal
 
 const sr = ScrollReveal({
