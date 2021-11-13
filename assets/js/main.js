559111a6fe3ec0e8b7b94f151112fea6840e9b5e
najimsadat <saidnajimullahs@yahoo.com>
2021-11-13 19:08:02 +0100

    last bugs fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,9 +39,7 @@ tabs.forEach((tab) => {
     const target = document.querySelector(tab.dataset.target);
 
     tabContents.forEach((tabContent) => {
-      console.log(tabContent);
       tabContent.classList.remove("qualification__active");
-      console.log(tabContent);
     });
     target.classList.add("qualification__active");
 
@@ -59,19 +57,21 @@ const sections = document.querySelectorAll("section[id]");
 
 function scrollActive() {
   const scrollY = window.pageYOffset;
+
   sections.forEach((current) => {
     const sectionHeight = current.offsetHeight;
     const sectionTop = current.offsetTop - 50;
-    sectionId = current.getAttribute("id");
+    let sectionId = current.getAttribute("id");
 
-    // if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-    //   document
-    //     .querySelector(".nav__menu a[href*=" + sectionId + "]")
-    //     .classList.add("active-link");
-    // } else {
-    //   document.querySelector(".nav__menu a[href=*" + sectionId + "]") +
-    //     "]".classList.remove("active-link");
-    // }
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
   });
 }
 
